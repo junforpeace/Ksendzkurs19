b01e20a7f68896545a21e88fd443c60ef8060f82
junforpeace <soapcream@gmail.com>
2021-09-19 23:06:20 +0200

    new commit wq
    exit

---
+++ Python/main.py
@@ -0,0 +1,42 @@
+name = "Oksana"
+age = 35
+weight = 55.5
+by = bytes('bytes', "utf-8")
+l1 = [1, 2, 3, 4, 5]
+tu = (12, 'ok', 2.13)
+num_set = {2, 5, 8}
+fr = frozenset('no')
+x = {}
+print(name, type(name))
+print(age, type(age))
+print(weight, type(weight))
+print(by, type(by))
+print(l1, type(l1))
+print(tu, type(tu))
+print(num_set, type(num_set))
+print(fr, type(fr))
+print(x, type(x))
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+d = 101
+e = 20
+f = d + e
+print(f)
+g = d / e
+print(g)
+h = d * e
+print(h)
+i = d // e
+print(i)
+j = d % e
+print(j)
+k = (7 + 12) ** 3 + 7 * 4 - 44 / 2**4
+print(k)
+
+
+
+
+
